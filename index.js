d9c2aab94cefa5d03fc0be7ef546fdcc790e580d
Mark Fisher <mark@yourtradesite.co.nz>
2018-02-26 14:50:38 +1300

    Added build route to create build files

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 var express = require('express');
 var wintersmith = require('wintersmith');
 var opn = require('opn');
+var fs = require('fs');
 
 var app = express();
 
 var env = wintersmith('./yts-wintersmith-template-copy/config.json');
+var clientId = '7891011';
 
 app.get('/', function(req, res) {
 	env.preview(function(err, server) {
 		if (err) throw err;
 		console.log('Server running');
-		opn('http://localhost:8080', {options: '--new-window'});
+		opn('http://localhost:8080');
 
 		res.send('View your preview at localhost:8080');
+	});
+});
+
+app.get('/build', function(req, res) {
+	fs.mkdir('./build/' + clientId, function(result){
+		if (errno = 'EEXIST') {
+			console.log('That location already exists. Replacing old files')
+		}
+		env.build('./build/' + clientId, function(err, result) {
+		console.log('Build Complete');
+		console.log('Result is: ' + result);
+
+		res.send('Your webpage has been built');
+		})
 	});
 });
 
